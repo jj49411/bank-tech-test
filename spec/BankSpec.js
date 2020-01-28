3a637751c392d8cf913f51035eff1707ee339443
Julie <j49411@gmail.com>
2020-01-28 23:40:46 +0000

    Refactor bank spec

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -1,15 +1,17 @@
 describe('Bank', function() {
   var bank;
 
-  it('can deposit money in an account', function() {
+  beforeEach(function() {
     bank = new Bank;
+  });
+
+  it('can deposit money in an account', function() {
     expect(bank.deposit(30)).toBe(30)
   });
 
   it('can add add on value in the account when deposit money', function() {
-    bank = new Bank;
     bank.deposit(40);
     expect(bank.deposit(50)).toBe(90)
   })
-  
+
 }); 
