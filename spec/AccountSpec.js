98923dd0c2c5f2bbc4c87f3321cdc681312a4345
Julie <j49411@gmail.com>
2020-01-29 00:06:01 +0000

    Update deposit function to add a transaction

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -1,27 +1,40 @@
 describe('Account', function() {
   var account = new Account;
+  var today;
 
   beforeEach(function() {
     account = new Account;
+    today = new Date(2020,1,14);
+    jasmine.clock().install();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
   });
 
   describe('#deposit', function(){
     it('can deposit money in an account', function() {
-      account.deposit(300)
+      account.deposit(300);
       expect(account.balance).toBe(300);
     });
 
     it('can add on value in the account when deposit money', function() {
       account.deposit(100);
-      account.deposit(400)
+      account.deposit(400);
       expect(account.balance).toBe(500);
+    });
+
+    it('should add a transaction when deposit money', function() {
+      jasmine.clock().mockDate(today);    
+      account.deposit(500);
+      expect(account.transactions).toContain(['14/02/2020', 500, 500])
     });
   });
 
   describe('#withdraw', function(){
     it('can withdraw money from an account', function() {
       account.deposit(1000);
-      account.withdraw(400)
+      account.withdraw(400);
       expect(account.balance).toBe(600);
     });
   });
--- src/Account.js
+++ src/Account.js
@@ -1,10 +1,13 @@
 function Account() {
   this.balance = 0;
+  this.transactions = [];
 };
 
 Account.prototype.deposit = function(amount) {
   var transaction = new Transaction;
+  var date = new DateFormat;
   this.balance += transaction.add(amount);
+  this.transactions.push([date.dateFormat(), amount, this.balance]);
 };
 
 Account.prototype.withdraw = function(amount) {
