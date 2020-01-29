c7b4cc17c325904d2a21d0528e1673479e683c8a
Julie <j49411@gmail.com>
2020-01-29 00:47:57 +0000

    Update feature tests

--- spec/FeatureTest.js
+++ spec/FeatureTest.js
@@ -1,11 +1,20 @@
 describe('Features', function() {
   var account;
+  var today;
   var amount;
 
   beforeEach(function() {
     account = new Account;
     amount = 500
+    today = new Date(2020,1,14)
+    jasmine.clock().install();
+    jasmine.clock().mockDate(today);  
   })
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
 
   it('user can deposit money in an account', function() {
     account.deposit(amount);
@@ -17,5 +26,16 @@ describe('Features', function() {
     account.withdraw(amount);
     expect(account.balance).toEqual(0)
   });
+
+  it('should record the date of deposit', function() {
+    account.deposit(amount);
+    expect(account.transactions.display()).toContain('14/02/2020')
+  });
+
+  it('user can print the bank statement', function() {
+    account.deposit(amount);
+    expect(account.transactions.display()).toContain('14/02/2020 || 500.00 || || 500.00')
+  });
+
 
 });
