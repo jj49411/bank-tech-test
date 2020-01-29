8d342c097dcba908581fcc9faa7de0eb82ede225
Julie <j49411@gmail.com>
2020-01-29 00:41:19 +0000

    Add unit test for log function

--- spec/StatementSpec.js
+++ spec/StatementSpec.js
@@ -1,16 +1,21 @@
 describe('Statement', function() {
   var account = new Account;
   var transactions;
+  var statement = new Statement;
 
   beforeEach(function() {
     transactions = [['14/02/2020', 1000, 1000], ['15/02/2020', -200, 800]]
     spyOn(account, 'transactions');
   });
 
+  it('should add new transaction in transactions', function() {
+    statement.log(transactions);
+    expect(statement.transactions).toContain([['14/02/2020', 1000, 1000], ['15/02/2020', -200, 800]])
+  });
+
   it('should display the header', function() {
     var statement = new Statement(transactions);
     expect(statement.display()).toContain('date || credit || debit || balance')
   });
-
 }); 
 
